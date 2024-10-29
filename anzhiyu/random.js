3acefc3fe33b9d63b53d833c4eee99b7005e9dbf
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-30 00:21:57 +0800

    Site updated: 2024-10-30 00:21:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/hello-world/","实验室/f2/","生活日常/life/","技术与编程/tech/","实验室/实验室/","技术与编程/test1/","实验室/test/"];function toRandomPost(){
+var posts=["实验室/f2/","uncategorized/hello-world/","技术与编程/tech/","技术与编程/test/","实验室/实验室/","生活日常/life/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

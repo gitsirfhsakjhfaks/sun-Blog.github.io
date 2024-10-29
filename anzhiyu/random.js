0a48543a78a25262207849d90683e5b1acca5f3d
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-29 11:51:54 +0800

    Site updated: 2024-10-29 11:51:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/hello-world/"];function toRandomPost(){
+var posts=["2024/10/28/hello-world/","2024/10/29/实验室/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

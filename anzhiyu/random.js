6467d5880db89dc146d79d911edf955014ce619c
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-30 00:12:42 +0800

    Site updated: 2024-10-30 00:12:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/hello-world/","生活日常/life/","技术与编程/tech/","实验室/f2/","实验室/实验室/"];function toRandomPost(){
+var posts=["实验室/f2/","uncategorized/hello-world/","生活日常/life/","实验室/实验室/","技术与编程/tech/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

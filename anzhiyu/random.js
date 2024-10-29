a8aaf6729445deb11b3d4ea28953b72cfb01805e
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-29 23:52:14 +0800

    Site updated: 2024-10-29 23:52:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["实验室/f2/","生活日常/life/","uncategorized/hello-world/","技术与编程/tech/","实验室/实验室/"];function toRandomPost(){
+var posts=["uncategorized/hello-world/","实验室/f2/","生活日常/life/","技术与编程/tech/","实验室/实验室/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

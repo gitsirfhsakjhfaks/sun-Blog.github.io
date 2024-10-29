2d7626b434c248c3991ae075d6f7312a946c9647
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-29 13:17:33 +0800

    Site updated: 2024-10-29 13:17:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/hello-world/"];function toRandomPost(){
+var posts=["uncategorized/hello-world/","uncategorized/f2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

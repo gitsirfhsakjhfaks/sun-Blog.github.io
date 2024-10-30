b4cdfb538ad997209640c87f78ae5a9214b7cb2c
gitsirfhsakjhfaks <2409593530@qq.com>
2024-10-30 14:52:46 +0800

    Site updated: 2024-10-30 14:52:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["实验室/f2/","uncategorized/hello-world/","生活日常/life/","实验室/实验室/","技术与编程/tech/","技术与编程/test2/","技术与编程/test/"];function toRandomPost(){
+var posts=["生活日常/life/","实验室/f2/","uncategorized/hello-world/","技术与编程/test2/","实验室/实验室/","技术与编程/tech/","技术与编程/test/"];function toRandomPost(){
     pjax.loadUrl('/sun-Blog.github.io/'+posts[Math.floor(Math.random() * posts.length)]);
   };
